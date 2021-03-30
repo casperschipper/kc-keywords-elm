379008878972148625d4b847fa8e7898038470a6
Caspers QuadMBP <casper.schipper@gmail.com>
2021-03-30 21:50:30 +0200

    download directly

--- data/fetcher.js
+++ data/fetcher.js
@@ -1,7 +1,3 @@
-let fetchData = function (url) {
-    return fetch(url).then(response => response.json());
-}
-
 let publishedUrl = "portal/search-result?fulltext=&title=&autocomplete=&keyword=&portal=6&statusprogress=0&statuspublished=0&statuspublished=1&includelimited=0&includelimited=1&includeprivate=0&type_research=research&resulttype=research&format=json&limit=250&page=0";
 
 let publishedUrl2 = "portal/search-result?fulltext=&title=&autocomplete=&keyword=&portal=6&statusprogress=0&statuspublished=0&statuspublished=1&includelimited=0&includelimited=1&includeprivate=0&type_research=research&resulttype=research&format=json&limit=250&page=1";
@@ -12,11 +8,34 @@ let lectorate = "portal/search-result?fulltext=&title=&autocomplete=&keyword=Kon
 
 let sonology = "portal/search-result?fulltext=&title=&autocomplete=&keyword=sonology&portal=6&statusprogress=0&statusprogress=1&statuspublished=0&includelimited=0&includeprivate=0&type_research=research&resulttype=research&format=json&limit=50&page=0";
 
+function download(filename, text) {
+    var pom = document.createElement('a');
+    pom.setAttribute('href', 'data:application/json;charset=utf-8,' + encodeURIComponent(text));
+    pom.setAttribute('download', filename);
+
+    if (document.createEvent) {
+        var event = document.createEvent('MouseEvents');
+        event.initEvent('click', true, true);
+        pom.dispatchEvent(event);
+    }
+    else {
+        pom.click();
+    }
+}
+
+let fetchData = function (url) {
+    return fetch(url).then(response => response.json());
+}
+
 let fetchAll = function () {
     let jsonPromises = [publishedUrl,publishedUrl2,publishedUrl3,sonology].map(fetchData) ;
     Promise.all(jsonPromises).then(values => { console.log(values);
 	                                       let flat = values.flat();
-					       console.log(JSON.stringify(flat)); });
+					       let json = JSON.stringify(flat);
+					       download('internal-research.json',json);
+					     });
 };
+
+
 
 fetchAll();
